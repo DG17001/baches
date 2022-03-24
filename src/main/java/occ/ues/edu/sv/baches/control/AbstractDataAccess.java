38eefa9f0b4938f75be7830d1606fb7f31fcf50b
DG17001 <dg17001@ues.edu.sv>
2022-03-24 12:16:25 -0600

    Metodos CRUD en clase Abstracta

--- src/main/java/occ/ues/edu/sv/baches/control/AbstractDataAccess.java
+++ src/main/java/occ/ues/edu/sv/baches/control/AbstractDataAccess.java
@@ -28,7 +28,7 @@ public abstract class AbstractDataAccess<T> implements Serializable {
 
     public abstract EntityManager getEntityManager();
 
-    public void crear(T nuevo) throws IllegalArgumentException,IllegalStateException{
+    public void crear(T nuevo) throws IllegalArgumentException, IllegalStateException {
         if (nuevo != null) {
             EntityManager em = null;
             try {
@@ -37,21 +37,17 @@ public abstract class AbstractDataAccess<T> implements Serializable {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
             if (em != null) {
-                try {
-                    em.persist(nuevo);
-                    return;
-                } catch (Exception e) {
-                    throw new IllegalStateException("No se puede obtener un ambito de persistencia");
-                }
+                em.persist(nuevo);
+                return;
             } else {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
         }
         throw new IllegalArgumentException();
     }
-    
-    public void eliminar(Object id) throws IllegalArgumentException,IllegalStateException{
-        if (id != null) {
+
+    public void eliminar(T registro) throws IllegalArgumentException, IllegalStateException {
+        if (registro != null) {
             EntityManager em = null;
             try {
                 em = getEntityManager();
@@ -59,20 +55,18 @@ public abstract class AbstractDataAccess<T> implements Serializable {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
             if (em != null) {
-                try {
-                    em.remove(em.getReference(clase, id));
-                    return;
-                } catch (Exception e) {
-                    throw new IllegalStateException("No se puede obtener un ambito de persistencia");
+                if (!em.contains(registro)) {
+                    registro = em.merge(registro);
                 }
+                em.remove(registro);
+                return;
             } else {
-                throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
         }
         throw new IllegalArgumentException();
     }
-    
-    public void modificar(T clase) throws IllegalArgumentException,IllegalStateException{
+
+    public void modificar(T clase) throws IllegalArgumentException, IllegalStateException {
         if (clase != null) {
             EntityManager em = null;
             try {
@@ -81,37 +75,54 @@ public abstract class AbstractDataAccess<T> implements Serializable {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
             if (em != null) {
-                try {
-                    em.merge(clase);
-                    return;
-                } catch (Exception e) {
-                    throw new IllegalStateException("No se puede obtener un ambito de persistencia");
-                }
+                em.merge(clase);
+                return;
             } else {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
         }
         throw new IllegalArgumentException();
     }
-    
-    public T findById(Object id) throws IllegalArgumentException,IllegalStateException{
+
+    public T findById(Object id) throws IllegalArgumentException, IllegalStateException {
         if (id != null) {
             EntityManager em = null;
             try {
                 em = getEntityManager();
             } catch (Exception e) {
-                throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
             if (em != null) {
-              return (T) em.find(clase, id);
+                return (T) em.find(clase, id);
             } else {
-               throw new IllegalStateException("No se puede obtener un ambito de persistencia");
+                throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
         }
         throw new IllegalArgumentException();
     }
-    
-    public List<T> findAll() throws IllegalStateException{
+
+    public List<T> findAll() throws IllegalStateException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+        } catch (Exception e) {
+        }
+        if (em != null) {
+            TypedQuery q = this.GenerarConsultaBase(em);
+            List salida = q.getResultList();
+
+            if (salida != null) {
+                return salida;
+            } else {
+                return Collections.EMPTY_LIST;
+            }
+
+        } else {
+            throw new IllegalStateException("No se puede obtener un ambito de persistencia");
+        }
+    }
+
+    public List<T> findRange(int first, int pageSize) throws IllegalArgumentException,IllegalStateException {
+        if (first != 0 && pageSize != 0) {
             EntityManager em = null;
             try {
                 em = getEntityManager();
@@ -119,73 +130,49 @@ public abstract class AbstractDataAccess<T> implements Serializable {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
             if (em != null) {
-                TypedQuery q=this.GenerarConsultaBase(em);
-                List salida=q.getResultList();
-                 
-                if (salida!=null) {
-                    return salida;    
-                }else{
+                TypedQuery q = this.GenerarConsultaBase(em);
+                q.setFirstResult(first);
+                q.setMaxResults(pageSize);
+                List<T> salida = q.getResultList();
+
+                if (salida != null) {
+                    return salida;
+                } else {
                     return Collections.EMPTY_LIST;
                 }
-                
             } else {
                 throw new IllegalStateException("No se puede obtener un ambito de persistencia");
             }
-    }
-    
-    public List<T> findRange(int first, int pageSize) throws  IllegalStateException{
-        if (first>0 && pageSize>0) {
-            EntityManager em=null;
-            try {
-                em=getEntityManager();
-            } catch (Exception e) {
-                throw new IllegalStateException("No se puede obtener un ambito de persistencia");
-            }
-            if (em!=null) {
-                TypedQuery q=this.GenerarConsultaBase(em);
-                q.setFirstResult(first);
-                q.setMaxResults(pageSize);
-                List<T> salida=q.getResultList();
-                
-                if(salida!=null){
-                    return salida;
-                }else{
-                    return Collections.EMPTY_LIST;
-                }
-            }else{
-                throw new IllegalStateException("No se puede obtener un ambito de persistencia");
-            }
-        }else{
-            throw new IllegalStateException("No se puede obtener un ambito de persistencia");
+        } else {
+            throw new IllegalArgumentException();
         }
     }
-    
-    public TypedQuery GenerarConsultaBase(EntityManager em){
-        if(em!=null){
-            CriteriaBuilder cb=em.getCriteriaBuilder();
-            CriteriaQuery cq=cb.createQuery(clase);
-            Root<T> raiz=cq.from(clase);
+
+    public TypedQuery GenerarConsultaBase(EntityManager em) {
+        if (em != null) {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery cq = cb.createQuery(clase);
+            Root<T> raiz = cq.from(clase);
             cq.select(raiz);
-            
+
             return em.createQuery(cq);
-        }else{
+        } else {
             throw new IllegalArgumentException("No se puede obtener un ambito de persistencia");
         }
     }
-    
-    public Long contar() throws IllegalStateException{
+
+    public Long contar() throws IllegalStateException {
         EntityManager em = null;
         try {
             em = getEntityManager();
         } catch (Exception e) {
-            throw new IllegalStateException("No se puede obtener un ambito de persistencia");
         }
-        if(em!=null){
-            CriteriaBuilder cb=em.getCriteriaBuilder();
-            CriteriaQuery<Long> cq=cb.createQuery(Long.class);
+        if (em != null) {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Long> cq = cb.createQuery(Long.class);
             cq.select(cb.count(cq.from(clase)));
             return em.createQuery(cq).getSingleResult();
-        }else{
+        } else {
             throw new IllegalStateException("No se puede obtener un ambito de persistencia");
         }
     }
